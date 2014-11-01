5fb1391060c12a22a3406862ad879192eda12812
Jaime Lyn Schatz <jaimelynschatz@gmail.com>
2014-10-31 22:00:20 -0700

    workaround for no css loading in heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -20,7 +20,7 @@ Rails.application.configure do
   # config.action_dispatch.rack_cache = true
 
   # Disable Rails's static asset server (Apache or nginx will already do this).
-  config.serve_static_assets = false
+  config.serve_static_assets = true # changed 10/31/14, attempt at no css in Heroku bug workaround
 
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
