7eee7e0f53dc66d981a7faa6e34d2da819cd9b8d
Jaime Lyn Schatz <jaimelynschatz@gmail.com>
2014-11-02 20:13:09 -0800

    another snippet from SO to fix no css in heroku issue

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -6,3 +6,8 @@ Rails.application.config.assets.version = '1.0'
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
+
+config.cache_classes = true
+config.serve_static_assets = true
+config.assets.compile = true
+config.assets.digest = true
