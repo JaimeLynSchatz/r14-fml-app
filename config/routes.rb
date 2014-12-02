7b5e4df1f221cf37602038e42e2a8612fa390c30
Jaime Lyn Schatz <jaimelynschatz@gmail.com>
2014-12-01 21:50:39 -0800

    add routing and todos view

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
-  root to: 'visitors#index'
+  root to: 'todos/index'
+
+  match "todos/delete" => "todos#delete", :via => [:delete], :as => :delete
+
+  match "todos/add" => "todos#add", :via => [:post], :as => :add
+
+  match "todos/complete" => 'todos#complete', :via => :post
+
 end
